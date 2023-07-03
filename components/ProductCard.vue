10b558bbf8ac372fadc3baa01ab9bbc94313d4c0
Wijayaac <wijayaac25@gmail.com>
2023-07-04 00:22:38 +0800

    Add : dynamic pages

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="col-12 col-md-3">
+  <div class="col-12 col-md-3 mb-4">
     <div class="card card--product">
       <div class="card-image">
         <img :src="product?.image" :alt="product?.title" />
       </div>
-      <h2>{{ product?.title }}</h2>
+      <nuxt-link :to="'/product/' + product?.id">{{ product?.title }}</nuxt-link>
       <p>{{ product?.price }}</p>
       <div class="rating">
         <span>Review : {{ product?.rating?.rate }}, </span>
@@ -14,21 +14,27 @@
   </div>
 </template>
 <script setup lang="ts">
-defineProps<{
-  product?: {
-    title?: string;
-    image?: string;
-    price?: string;
-    rating?: {
-      rate?: number;
-      count?: number;
-    };
+interface Product {
+  id?: number;
+  title?: string;
+  image?: string;
+  price?: string;
+  rating?: {
+    rate?: number;
+    count?: number;
   };
+}
+
+const props = defineProps<{
+  product: Product;
 }>();
+const slug = computed(() => props.product?.title?.replace(/\//g, "").replace(/\s+/g, "-").toLowerCase());
 </script>
 
 <style lang="scss">
 .card {
+  height: 100%;
+  padding-bottom: 40px;
   &-image {
     position: relative;
     width: 100%;
---
+++ pages/product/[id].vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="main-product row">
+      <div class="col-12 col-md-6">
+        <div class="main-product-image">
+          <img :src="product.image" :alt="product.title" class="img-fluid" />
+        </div>
+      </div>
+      <div class="main-product-content col-12 col-md-6">
+        <h1>{{ product.title }}</h1>
+        <p>{{ product.price }}</p>
+        <p>{{ product.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const route = useRoute();
+const config = useRuntimeConfig();
+
+const { data: product, pending, error } = await useAsyncData("product", () => $fetch(`${config.public.apiBase}/products/${route.params.id}`));
+</script>
