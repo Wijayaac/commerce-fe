66970d1b8646527fdb5f6724c16df02fa91eb7ba
Wijayaac <wijayaac25@gmail.com>
2023-07-04 22:32:37 +0800

    Update : folder structure & handler method

---
+++ components/ui/Button.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="btn">
+    <slot></slot>
+  </button>
+</template>
+
+<style lang="css">
+.btn {
+  background-color: red;
+  color: white;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <UiButton @click="handleClick"> Click Me! </UiButton>
+
     <Hero />
     <Products :products="products" />
   </div>
@@ -11,4 +13,8 @@ const { data: products } = await useAsyncData("product", async () => {
   const { data } = await client.from("products").select("*");
   return data;
 });
+
+const handleClick = () => {
+  console.log("button diclick");
+};
 </script>
